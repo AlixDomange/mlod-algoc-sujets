1688de9518b20d5e5c15ed261fd0d881922bce9b
Alix <alix.domange@gmail.com>
2023-09-20 09:30:36 +0200

    echangeContenu

--- TP-02-pointeurs/01-echangeContenu/main.c
+++ TP-02-pointeurs/01-echangeContenu/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "echangeContenu.h"
 
+void echangeContenu( int *a, int *b){
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
 int main(void)
 {
 	int a=4,b=14;
 
 	printf ("a = %d\tb = %d\n", a, b);
 
-	// TODO
+	echangeContenu(&a, &b);
 
 	printf ("a et b echanges \n");
 	printf ("a = %d\tb = %d\n", a, b);
