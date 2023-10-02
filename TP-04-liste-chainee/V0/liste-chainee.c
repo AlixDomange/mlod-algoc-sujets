3ba229efd3b31b24f3238b81d4170940d32bbfd0
Alix <alix.domange@gmail.com>
2023-10-02 17:01:53 +0200

    TP4 V0

--- TP-04-liste-chainee/V0/liste-chainee.c
+++ TP-04-liste-chainee/V0/liste-chainee.c
@@ -11,12 +11,19 @@ bool estVide(Liste l) {
 
 // créer une liste d'un seul élément contenant la valeur v
 Liste creer(Element v){
-	return TODO;
+	Liste l;
+	l = (Liste)malloc(sizeof(Cellule));
+	l->val= v;
+	l->suiv=NULL;
+	return l;
 }
 
 // ajoute l'élément v en tete de la liste l
 Liste ajoutTete(Element v, Liste l) {
-	return TODO;
+	Cellule *cell;
+	cell=creer(v);
+	cell->suiv=l;
+	return cell;
 }
 
 
@@ -30,12 +37,19 @@ void afficheElement(Element e) {
 // Attention la liste peut être vide !
 // version itérative
 void afficheListe_i(Liste l) {
-	TODO;
+	Cellule *c=l;
+	while(c!=NULL){
+		afficheElement(c->val);
+		c=c->suiv;
+	}
 }
 
 // version recursive
 void afficheListe_r(Liste l) {
-	TODO;
+	if(l!=NULL){
+		afficheElement(l->val);
+		afficheListe_r(l->suiv);
+	}
 }
 
 void detruireElement(Element e) {}
@@ -43,18 +57,38 @@ void detruireElement(Element e) {}
 // Détruit tous les éléments de la liste l
 // version itérative
 void detruire_i(Liste l) {
-	TODO;
+	Liste suivant, courant = l;
+	while(courant!=NULL){
+		suivant=courant->suiv;
+		detruireElement(courant->val);
+		free(courant);
+		courant = suivant;
+	}
 }
 
 // version récursive
 void detruire_r(Liste l) {
-	TODO;
+	if(l!=NULL){
+		detruire_r(l->suiv);
+		detruireElement(l->val);
+		free(l);
+	}
 }
 
 // retourne la liste dans laquelle l'élément v a été ajouté en fin
 // version itérative
 Liste ajoutFin_i(Element v, Liste l) {
-	return TODO;
+	Liste elem = creer(v);
+	if(l==NULL){
+		return elem;
+	}
+	while(l->suiv!=NULL){
+		l=l->suiv;
+	}
+	l->suiv=cell;
+	cell->val=v;
+	p->suiv=elem;
+	return l;
 }
 
 // version recursive
@@ -70,6 +104,11 @@ bool equalsElement(Element e1, Element e2){
 // Retourne un pointeur sur l'élément de la liste l contenant la valeur v ou NULL
 // version itérative
 Liste cherche_i(Element v,Liste l) {
+	Liste p =l;
+	while(c!=NULL && !equalsElement(p->val,v)){
+		p=p->suiv;
+	}
+
 	return TODO;
 }
 
